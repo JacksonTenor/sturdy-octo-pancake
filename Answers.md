99dd943994224de893f9ee428e2c925965393a30
michael-small <small203@morris.umn.edu>
2019-10-27 22:34:32 -0500

    Done?

--- Answers.md
+++ Answers.md
@@ -1,8 +1,21 @@
-1. **Develop an algorithm that, given preferences for N programmers and
-N companies, finds a satisfactory pairing. If there is more than one
-satisfactory pairing, you need to find just one.**
+Clarifying comments in code
 
-2.  **Implement your algorithm and test it on several cases of preferences. It does not matter how your algorithm takes data, but you should clearly explain this in the documentation so that we know how to test it. Also make sure to document all your test cases and results. Check that the pairings found by your program are satisfactory (write a method to do this to save yourself some time).** The tests run 
+1. **Develop an algorithm that, given preferences for N programmers and N companies, finds a satisfactory pairing. If 
+there is more than one satisfactory pairing, you need to find just one.** We find the most optimal pairing. This is 
+faciliated in the `ALGORITHM` section of the code. In essence, the algorithm runs from the perspective of the companies.
+A company picks their current most favorite candidate, hence it is represented by a queue of their programmer preferences,
+with most preferred at the head. First one gets first pick. That employee (employee preferences are a stack, with
+their highest priority at the bottom of the stack) is tentatively claimed and their lower preference companies than that
+current company are popped off. The employees do not necessarily have the "bargaining power" to get to their top pick. 
+That is fine as long as the company is getting their highest available priority employee, hence overall the setup is 
+satisfactory enough. If another company comes along and tries to claim that employee, the company that is more preferred 
+by that employee is picked, the employee stack pops off the less preferred company and ones below that, and the companies 
+knocked off have to find their next best favorite through that same process.
+
+2.  **Implement your algorithm and test it on several cases of preferences. It does not matter how your algorithm takes 
+data, but you should clearly explain this in the documentation so that we know how to test it. Also make sure to document 
+all your test cases and results. Check that the pairings found by your program are satisfactory (write a method to do 
+this to save yourself some time).** The tests run 
 themselves as many times as `runXTests` in the `for` loop specifies.
 These tests run automatically in main. The console will print them out,
 separated by single lines. Each test run creates a grid of n^2 size, with
@@ -10,13 +23,20 @@ n ranging from 3 to 9 depending on the random outcome. n down is the
 list of preferences of either the companies or programmers, and n across
 is the list of each company/programmer. The console shows the preferences
 of both, in order from left to right within each company/programmer list
-from highest to lowest ranking of the respective other side. Each test
-also shows the solution and whether it is valid by out tests. Solutions
+from highest to lowest ranking of the respective other group. Each test
+also shows the solution and whether it is satisfactory by out tests. Solutions
 are checked via `checkSolution()` with the data of each random test case.
 The companies and programmer preferences are checked against the our
-algorithm's outputted solution. 
+algorithm's outputted solution. If there is a situation where an employee wants a swap while a company also wants a swap,
+the checker would fail and out algorithm would fail. Running 100 tests a few times does not abort/break the tests ever.
+*Example of a single 3 company/3 programmer run with non-random assignments available at the top of the console printout. For
+the sake of the grader, `runXTests` has been tuned down to 5 by default.*
 
-3. **Explain why your algorithm is correct (i.e. it always stops and outputs a satisfactory pairing). You don’t need to go into low-level details of your program, but your argument must be precise enough to convince someone who has not seen your program before that it is indeed correct.** 
+3. **Explain why your algorithm is correct (i.e. it always stops and outputs a satisfactory pairing). You don’t need to 
+go into low-level details of your program, but your argument must be precise enough to convince someone who has not seen y
+our program before that it is indeed correct.** At no point in hundreds of tests does both a company and programmer at
+any single instance insist on a swap to better satisfy their preferences, so by the definition of the problem parameters,
+it is correct. 
 
 4. **Find the efficiency of your algorithm in the worst case, justify your
 answer.** O(n^2). The problem space of the selections of the preferences
--- src/Hiring.java
+++ src/Hiring.java
@@ -5,31 +5,28 @@ public class Hiring {
     private static ArrayList<Stack<Integer>> programmers = new ArrayList<>();
 
     public static void main(String[] args){
-        //Integer[][] companyArr = {{2,5,1,3,4}, {1,2,3,4,5}, {5,3,2,1,4}, {1,3,2,4,5}, {2,3,5,4,1}};
-        //Integer[][] programmerArr = {{5,1,4,2,3},{4,5,2,1,3},{4,2,3,5,1},{3,2,4,1,5},{1,4,2,3,5}};
-
-//        Integer[][] companyArr = {{3,2,1}, {3,2,1}, {2,3,1}};
-//        Integer[][] programmerArr = {{2,1,3}, {1,2,3}, {2,1,3}};
-
         Integer[][] companyArr = {{2,3,1}, {2,3,1}, {1,3,2}};
         Integer[][] programmerArr = {{1,2,3}, {3,2,1}, {2,1,3}};
 
         companies = makePreferencesQueue(companyArr);  //DATA: Companies is a Queue
         programmers = makePreferencesStack(programmerArr);  //DATA: Programmers is a stack
 
-        System.out.println("Companies: " + companies);
-        System.out.println("Programmers: " + programmers);
+        System.out.println("Company A, B, C; read left to right amongst each as highest priority");
+        System.out.println("Companies: " + companies + "\n");
+        System.out.println("Programmer 1, 2, 3; read left to right amongst each as highest priority");
+        System.out.println("Programmers: " + programmers+ "\n");
         Integer[] solution = hireEmployees(companies, programmers);
-        System.out.println("a " + checkSolution(companies,programmers, solution));
-        System.out.println("solution: " + Arrays.toString(solution));
+        System.out.println("Solution is valid: " + checkSolution(companies,programmers, solution) + "\n");
+        System.out.println("Solution from the perspective of the employer's preferences of programmers");
+        System.out.println("solution: " + Arrays.toString(solution) + "\n");
 
         System.out.println("----------------------------");
         System.out.println("----------------------------");
         System.out.println("random data below, separated by one line each");
         System.out.println("----------------------------");
-        System.out.println("----------------------------");
+        System.out.println("----------------------------" + "\n");
         //Generate random data
-        for(int runXTests=0; runXTests<100; runXTests++) { //TESTING: Change R to change amount of test runs
+        for(int runXTests=0; runXTests<5; runXTests++) { //TESTING: Change R to change amount of test runs
             Random rng = new Random();
             int size = rng.nextInt(7) + 3;  //TESTING: Determines amount of companies/programmers, from
                                                   //sizes 3 to 9 (nextInt from 0 [incl] to specified number [excl])
@@ -65,6 +62,10 @@ public class Hiring {
 
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
+    //DATA: Makes preference lists for the company queue and programmer stack
+    //-----------------------------------------------------------------------------------------------------------------
+
     static ArrayList<Queue<Integer>> makePreferencesQueue(Integer[][] arr){
         ArrayList<Queue<Integer>> list = new ArrayList<>();
         for(Integer[] innerArr: arr){
@@ -85,6 +86,11 @@ public class Hiring {
         return list;
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
+    //ALGORITHM: Does the picking of employees to each company
+    //-----------------------------------------------------------------------------------------------------------------
+
+    // Facilitates the macro level of hiring employees
     static Integer[] hireEmployees(ArrayList<Queue<Integer>> companies, ArrayList<Stack<Integer>> programmers){
         Integer[] hired = new Integer[companies.size()];
         for(int i=0; i<companies.size(); i++){
@@ -94,6 +100,8 @@ public class Hiring {
         return hired;
     }
 
+    // The meat of the hiring process. Company claims employee at the head of their remaining queue, and claimed employees
+    // knock companies that are less desired off of their stack
     static void hireEmployee(Queue<Integer> company, ArrayList<Stack<Integer>> programmers,  Integer[] hired, Integer companyIndex){
         Integer employeeNum = null;
         while(employeeNum == null) {
@@ -101,15 +109,17 @@ public class Hiring {
             if (candidate.contains(companyIndex + 1)) { //determines if the programmer would prefer this company to their current, or always true if programmer has no job.
                 employeeNum = company.peek();
                 while (candidate.peek() != companyIndex + 1) {
-                    candidate.pop();
+                    candidate.pop();  // If there are company choices less desirable to an employee, pop off their preference stack
                 }
             } else {
-                company.poll();
+                company.poll();  // Narrows down available employees in a company's want list
             }
         }
-        replaceEmployee(hired, companyIndex, employeeNum,  companies, programmers);
+        replaceEmployee(hired, companyIndex, employeeNum,  companies, programmers);  // figures out conflicts of companies wanting employees
     }
 
+    // Where conflicts of what company wants which employee is settled, ending in hireEmployee narrowing down preferences
+    // via employee pops or company polls
     static void replaceEmployee(Integer[] hired, Integer companyHiring, Integer programmerHired, ArrayList<Queue<Integer>> companies, ArrayList<Stack<Integer>> programmers){
         Integer nowHiring = null;
         for (int j = 0; j < hired.length; j++) {
@@ -143,7 +153,7 @@ public class Hiring {
         return true;
     }
 
-    // As long as the head of the queue 
+    // If the company prefers someone higher up in the queue than the actual programmer option, the company wants a swap
     static boolean companyWantsSwap(Queue<Integer> icompany, int current, int option){
         Queue<Integer> company = new LinkedList<>(icompany);
         while(!company.isEmpty()){
@@ -158,7 +168,7 @@ public class Hiring {
         return false;
     }
 
-    // If
+    // If a programmer can be offered better (higher in the stack), they would want to swap in that deal
     static boolean programmerWantsSwap(Stack<Integer> iprogrammer, int current, int option){
         Stack<Integer> programmer = (Stack<Integer>) iprogrammer.clone();
         while(!programmer.isEmpty()){
@@ -172,8 +182,9 @@ public class Hiring {
         }
         return false;
     }
-    //-----------------------------------------------------------------------------------------------------------------
 
+    // Maps the value of the solution (given from perspective of company picks of programmers) to what company a programmer
+    // is in, for checking if a programmer wants a swap from the current solution.
     static int getPosition(Integer[] arr, Integer value){
         for(int i=0; i<arr.length; i++){
             if(arr[i].equals(value)){
@@ -182,4 +193,6 @@ public class Hiring {
         }
         return -1;
     }
+    //-----------------------------------------------------------------------------------------------------------------
+
 }
